35bbc2450ab6b760d139fa9de19bd8b6e5a0429a
liyu <liyu_work@outlook.com>
2020-07-26 22:47:12 +0800

    recover data

--- main.go
+++ main.go
@@ -1,22 +1,32 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package main
 
-import "blucruz/collect-resources/cmd"
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
 
 func main() {
-	cmd.Execute()
+
+	// cmd.Execute()
+	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	api := clientset.CoreV1()
+	pods, err := api.Pods("").List(context.TODO(), metav1.ListOptions{})
+
+	for _, pod := range pods.Items {
+		fmt.Println(pod.Spec.Containers[0].Name, "--", pod.Spec.Containers[0].Resources.Requests.Cpu())
+	}
 }
